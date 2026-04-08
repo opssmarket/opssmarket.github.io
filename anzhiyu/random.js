eb60f3299d9b9e861fff3e012ef246440efb2a5a
opssmarket <info@opssmarket.com>
2026-04-08 16:09:51 +0200

    Site updated: 2026-04-08 16:09:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/04/08/BLOG/","2026/04/08/hello-world/","2026/04/08/这是我的/"];function toRandomPost(){
+var posts=["2026/04/08/OP-MIKE/","2026/04/08/hiokokokok/","2026/04/08/这是我的/","2026/04/08/BLOG/","2026/04/08/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
