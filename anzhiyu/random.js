9bdf06275c095e694ff36d6b23ce9c14f5e17cc2
opssmarket <info@opssmarket.com>
2026-04-22 22:24:45 +0200

    Site updated: 2026-04-22 22:24:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/04/08/OP-MIKE/","2026/04/22/CAZZO/"];function toRandomPost(){
+var posts=["2026/04/08/OP-MIKE/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
